fe5f32ff7c968ad7aec4d72bd2166b096ac68268
suhail891 <aazam.asdf@gmail.com>
2026-04-05 03:57:54 +0000

    Return_Length_Argument_Passing Example

---
+++ Return_Length_Argument_Passing.js
@@ -0,0 +1,10 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+ argumentsLength(1, 2, 3); // 3
+console.log(argumentsLength(1, 2, 3,5)) // 4
